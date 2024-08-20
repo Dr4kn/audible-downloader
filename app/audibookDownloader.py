a5721f36f7171387a34b347d39eba0ef4e8e9265
Dr4kn <servicesDrkn@gmail.com>
2024-08-20 19:08:20 +0200

    download and encoding aax files to m4b works

--- app/audibookDownloader.py
+++ app/audibookDownloader.py
@@ -3,54 +3,69 @@ import os
 import subprocess
 import sqlite3
 import csv
-import re
+
+con = sqlite3.connect("audiobooks.db")
+with con:
+	con.execute("""CREATE TABLE IF NOT EXISTS audiobooks (
+                asin TEXT UNIQUE,
+                title TEXT NOT NULL,
+                subtitle TEXT,
+				purchase_date TEXT,
+                downloaded INT
+			 	authors
+			 	series_title
+        );""")
 
 # audible_directory = "/config"
 audible_directory = "/home/adrian/.audible"
+audiobook_directory = "/home/adrian/Music/"
 
-def get_activation_bytes():
-	results = [each for each in os.listdir(audible_directory) if each.endswith('.json')]
-	file = json.load(open(audible_directory + "/" + results[0]))
+# program exits and errors if it can't get the activation bytes
+results = [each for each in os.listdir(audible_directory) if each.endswith('.json')]
+activation_bytes = json.load(open(audible_directory + "/" + results[0]))["activation_bytes"]
 
-	return(file["activation_bytes"])
 
-def get_all_audiblefiles():
-	return [each for each in os.listdir(audible_directory) if each.endswith('.aax')]
-	
-def saveDownloadedFiles(audiblefiles):
-	con = sqlite3.connect("audiobooks.db")
-	cur.execute("CREATE TABLE audiobooks(asin, title, purchase_date)")
-
-def test():
-	audiobooks = [each for each in os.listdir(audible_directory) if each.endswith('.aax')]
+# get library from audible cli
+# update db with all new titles
+def update_titles():
+	subprocess.run(["audible", "library", "export"])
 	file = lines = open("library.tsv")
 	reader = csv.DictReader(file, delimiter='\t')
-	audio = audiobooks[0].split('-')[0]
+	cur = con.cursor()
+
 	for row in reader:
-		name = row['title'] + row['subtitle']
-		name = re.sub(r'[ ,]+', '_', name)
-		print(name)
-		print(audio)
-		print("\n")
-		if audio == name:
-			print(name)
+		values = [row['asin'], row['title'], row['subtitle'], row['purchase_date'], 0]
+		if cur.execute('SELECT * FROM audiobooks WHERE asin=?', [row['asin']]).fetchone() is None:
+			cur.execute('insert into audiobooks values(?, ?, ?, ?, ?)', values)
 
-	# for audio in audiobooks:
-	# 	print(audio.split('-')[0])
-	# print(audiobooks)
+	con.commit()
 
+def download_new_titles():
+	cur = con.cursor()
+	to_download = cur.execute('SELECT asin FROM audiobooks WHERE downloaded=?', [0]).fetchall()
 
-
-def main():
-	# the programm kills itself if it can't find it, which is pretty much what I want regardless
-	test()
-	# activation_bytes = get_activation_bytes()
-
+	if not to_download:
+		exit()
 	
-	# for audiofile in get_all_audiblefiles():
-	# 	audiobook = audible_directory + "/" + audiofile
-	# 	subprocess.run(["ffmpeg", "-activation_bytes", activation_bytes, "-i", audiobook, "-c", "copy", audiobook[:-3] + "m4b"])
-	# 	print("finished")
+	for asin in to_download:
+		print("before")
+		print(asin[0])
+		subprocess.run(["audible", "download", "-a", asin[0], "--aax"])
+		cur.execute('UPDATE audiobooks SET downloaded = 1 WHERE asin = ?', asin)
+		con.commit()
+		# if downloaded stuff is folder delete
+		print("after")
+		audiobooks = [each for each in os.listdir(audible_directory) if each.endswith('.aax')]
+		for audiobook in audiobooks:
+			src = audible_directory + "/" + audiobook
+			des = audiobook_directory + audiobook[:-3] + "m4b"
+			subprocess.run(["ffmpeg", "-activation_bytes", activation_bytes, "-i", src, "-c", "copy", des])
+			os.remove(src)
+		break
+		
+def main():
+	# update_titles()
+	download_new_titles()
 
 if __name__ == "__main__":
 	main()
