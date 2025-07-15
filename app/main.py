7e3d3e69cef0ee7d9d9ff1a94c852dd4b52fe19c
Dr4kn <servicesDrkn@gmail.com>
2025-07-15 12:39:27 +0200

    refactor: switched from os.path to pathlib for better cross os support

--- app/main.py
+++ app/main.py
@@ -1,32 +1,35 @@
 from audibleDownloader import Authentication
 from audibleDownloader import Book
 from audibleDownloader import Library
-from audibleDownloader import Downloader
 from audibleDownloader import Status
 import os
+from pathlib import Path
 
-path = os.path.expanduser("~/.config/audible/")
-auth = Authentication(path).get_authentication()
-library = Library(auth, path)
-# library.add_books()
+path = os.path.expanduser("~/.config/audible")
+p = Path(path)
+auth = Authentication(p).get_authentication()
+library = Library(auth, p)
+library.add_books()
 lib_list = library.get_undownloaded_books()
 
 while len(lib_list) > 0:
     book = lib_list.pop(0)
     print(book.asin)
-    downloader = Downloader(path, book.asin)
+    book.set_path(p)
 
-    result = downloader.download()
+    result = book.download()
     if result == Status.ERROR:
         break
     library.set_book_as_downloaded(book.asin)
 
     # TODO make a way of converting downloaded but not converted books. 
     # Like when the download finished but the convertion stopped for some reason
-    result = downloader.convert()
+    result = book.convert()
     if result == Status.ERROR: 
         break
     if result == Status.NOT_DOWNLOADED:
         library.set_book_as_not_downloaded(book.asin)
         continue
     library.set_book_as_converted(book.asin)
+    book.set_metadata()
+    break
